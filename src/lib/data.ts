e22d89dddebd730cb651ff86a299ead3e899fa44
CrowHugin <kuntz.bastien@gmail.com>
2025-06-25 21:42:20 +0200

    Changed Education section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -46,15 +46,12 @@ export const workExperience = [
 
 export const education = [
   {
-    institution: "Sri Krishna College of Engineering and Technology",
-    location: "Coimbatore, India",
-    degree: "B Tech Information Technology",
-    period: "Jun 2021 - Jun 2025",
+    institution: "EPF Ecole d'ingénieurs",
+    location: "Cachan, France",
+    degree: "Bachelor: Systemes d’information et stratégie marketing",
+    period: "Sep 2022 - Aug 2025",
     achievements: [
-      "President of the Department of Information Technology",
-      "G20 student delegate",
-      "Organized multiple technical workshops and events",
-      "Represented my college in multiple national and international hackathons and won.",
+      "Work on multiple languages as python, html, excel VBA ...",
     ],
   },
 ];
