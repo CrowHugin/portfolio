321bc90a907a2d6aae2a51c7256c432dd1457f82
CrowHugin <kuntz.bastien@gmail.com>
2025-06-25 22:23:51 +0200

    Corrected projects section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -57,30 +57,24 @@ export const skills = {
   ],
   learning: ["C", "Docker",],
   Languages: ["French: native", "English: B2"],
-  Interest: ["Climbing (indoor)","Chess"],
 };
 
 export const projects = [
   {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
+    title: "C learning",
+    github: "https://github.com/CrowHugin/C-learning",
     description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+      "Currently learning C thanks to a book written by Frédéric Drouillon.",
+      "Only works on basics things for now not some complexe project.",
     ],
   },
   {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
+    title: "Password Manager",
+    github: "https://github.com/CrowHugin/password-manager",
     description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+      "A project to expend my programming knowledge.",
+      "Will work on differents ways, for now the module one is on.",
+      "Later, there will be a website, an web extension & an desktop app.",
     ],
   },
 ];
