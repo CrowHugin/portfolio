8bb010d58d764f03246d1a37ea5485f7e499dc14
CrowHugin <kuntz.bastien@proton.me>
2026-06-27 14:01:19 +0200

    Added achievement part to Ecole 42 section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -27,6 +27,9 @@ export const education = [
     degree: "Ecole 42",
     location: "Paris, France",
     period: "April 2025 - ongoing",
+    acheevements: [
+      "Passed through the 'Piscine' and integrated the school at part of the new cursus."
+    ]
   },
   {
     institution: "EPF Ecole d'ingénieurs",
